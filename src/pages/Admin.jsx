3d3217b45ed20a6b4b32adc5281b12df6fd5dbef
王康 <wk1507341428@gmail.com>
2021-07-28 11:56:36 +0800

    add http

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -2,22 +2,14 @@ import React from 'react';
 import { HeartTwoTone, SmileTwoTone } from '@ant-design/icons';
 import { Card, Typography, Alert } from 'antd';
 import { PageHeaderWrapper } from '@ant-design/pro-layout';
-import { useIntl } from 'umi';
 export default () => {
-  const intl = useIntl();
   return (
     <PageHeaderWrapper
-      content={intl.formatMessage({
-        id: '二极管理页',
-        defaultMessage: 'This page can only be viewed by admin ～',
-      })}
+      content={'This page can only be viewed by admin ～'}
     >
       <Card>
         <Alert
-          message={intl.formatMessage({
-            id: 'pages.welcome.alertMessage',
-            defaultMessage: 'Faster and stronger heavy-duty components have been released.',
-          })}
+          message={'Faster and stronger heavy-duty components have been released.'}
           type="success"
           showIcon
           banner
